8bdf748162d633956c84543bf0b9213c9bfe903a
swetac2 <103720160+swetac2@users.noreply.github.com>
2022-08-19 18:26:20 -0400

    Added github workflow

--- Environment/config.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "nonprod-acs2-assignment"       // Bucket where to SAVE Terraform State
-    key    = "dev-network/terraform.tfstate" // Object name in the bucket to SAVE Terraform State
-    region = "us-east-1"                     // Region where bucket is created
-  }
-}
